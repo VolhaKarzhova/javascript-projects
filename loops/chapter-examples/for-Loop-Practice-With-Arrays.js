03222909800dfb8804ad10a4a72d3178158e84ba
Olga Karzhova <olgakorzhova@Olgas-Air.lan>
2024-05-11 16:53:40 -0500

    add loop for arrays

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,7 @@
 // create an array variable containing the names
+const names = ['Anna', 'Maria', 'Varvara'];
 
 // write a for loop that prints each name on a different line
+for( let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
