45c4e24a8c2487224654cceef5f7ea989a1234e5
Olga Karzhova <olgakorzhova@Olgas-Air.lan>
2024-06-05 11:03:01 -0500

    add examples section

--- objects-and-math/chapter-examples/ForInLoop.js
+++ objects-and-math/chapter-examples/ForInLoop.js
@@ -7,3 +7,24 @@ let tortoiseOne = {
 };
 
 // Using a for..in loop, iterate through each property in the tortoiseOne object and print the value to the console.
+
+for (item in tortoiseOne) {
+   console.log(item);
+}
+
+console.log("experiments below");
+console.log(Math.floor(-3.87));
+console.log(Math.random(-3.87));
+console.log(Math.round(-3.87));
+console.log(Math.trunc(-3.87));
+
+
+
+let num = Math.floor(Math.random()*10);
+
+console.log(num + " from 0 to 9");
+
+
+let num1 = Math.round(Math.random()*10);
+
+console.log(num1 + " from 0 to 10");
--- objects-and-math/chapter-examples/KindnessSelection.js
+++ objects-and-math/chapter-examples/KindnessSelection.js
@@ -13,5 +13,27 @@ function randomSelection(arr){
  
  //Experiment with the code above. Try to:
  //a) Print 3 random selections from each array.
+ for (i=0; i < 3; i++){
+  console.log(randomSelection(happiness));
+  console.log(randomSelection(words));
+}
  //b) Have the code randomly pick one array, and then print 2 random items from it.
+
+ let arrays = [];
+ arrays.push(happiness);
+ arrays.push(words);
+ let randomArray = randomSelection(arrays);
+ console.log("Selected array: ".concat(randomArray));
+
+ for (i=0; i < 2; i++){
+  console.log(randomSelection(randomArray));
+}
+
+ 
  //c) Create a new array, then fill it with one random item from words and happiness. Print the new array.
+
+ let newArray = [];
+ newArray.push(randomSelection(happiness));
+ newArray.push(randomSelection(words));
+ console.log(newArray);
+
