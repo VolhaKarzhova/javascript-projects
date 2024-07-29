a4e945ae1b305e0e1388d19ae9b62958f2b289ca
Olga Karzhova <olgakorzhova@Olgas-MacBook-Air.local>
2024-07-29 18:58:21 +0300

    add tasks

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,6 +4,33 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
+
+   
+
+    button.addEventListener("click", function() {
+        paragraph.innerHTML = "Houston, we have liftoff!";
+    });
+
+    //solution from the book:
+    // button.addEventListener("click", event => {
+    //     paragraph.innerHTML = "Houston, we have liftoff!";
+    // });
+
+    missionAbort.addEventListener("mouseover", event => {
+        missionAbort.style.backgroundColor = "red";
+    })
+
+    missionAbort.addEventListener("mouseleave", function() {
+        missionAbort.style.backgroundColor = "";
+    })
+
+    missionAbort.addEventListener("click", event => {
+        const isAbortMission = window.confirm("Are you sure you want to abort the mission?")
+        if(isAbortMission) {
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home";
+        }
+    })
+    
     
 }
 
